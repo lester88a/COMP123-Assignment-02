3870fae35b1e6b17e1a367848f8b43e01d2084db
lester88a <lester88a@gmail.com>
2015-02-03 21:01:13 -0500

    Create private properties, Constructor and RandomPowers

--- COMP-123 Assignment 02/SuperHero.cs
+++ COMP-123 Assignment 02/SuperHero.cs
@@ -8,6 +8,24 @@ namespace COMP_123_Assignment_02
 {
     class SuperHero:Hero
     {
-        
+        //create private properties
+        private string[] superPowers;
+
+        //create constructor
+        public SuperHero(string name, int strength, int speed, int health):base(name)
+        {
+            generateRandomPowers();
+        }
+
+        //private Method
+        private void generateRandomPowers()
+        {
+            superPowers[0] = "Super Speed";
+            superPowers[1] = "Super Strength";
+            superPowers[2] = "Body Armour";
+            superPowers[3] = "Flight";
+            superPowers[4] = "Fire Generation";
+            superPowers[5] = "Weather Control";
+        }
     }
 }
